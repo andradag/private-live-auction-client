f714f979d30714b8f2cf92cfce5b5c5d018d54c9
Darion Richards <darionrichards@outlook.com>
2022-03-09 15:45:05 +0000

    Refactored SignUp page

--- src/components/pages/SignUp/index.js
+++ src/components/pages/SignUp/index.js
@@ -1,194 +1,38 @@
-import { useMutation } from "@apollo/client";
-import { useForm } from "react-hook-form";
-import { Link as RouterLink, useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
-import LoadingButton from "@mui/lab/LoadingButton";
-import ErrorIcon from "@mui/icons-material/Error";
 import Divider from "@mui/material/Divider";
 
-import { SIGNUP } from "../../../mutations";
+import {SignUpForm} from "../../SignUpForm";
 
-export const SignUpForm = () => {
-  const [executeSignUp, { loading, error }] = useMutation(SIGNUP);
+export const SignUp = () => {
+	const styles = {
+		header: {
+			paddingTop: 2,
+			paddingBottom: 2,
+			backgroundColor: "#F8F8FF",
+			mx: "auto",
+			width: 700,
+		},
+	};
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+	return (
+		<Box sx={styles.container}>
+			<Typography
+				variant="h4"
+				gutterBottom
+				component="h1"
+				align="center"
+				sx={styles.header}
+			>
+				Signup Form
+			</Typography>
 
-  const navigate = useNavigate();
+			<Typography variant="h6" gutterBottom component="h1" align="center">
+				Create an account
+			</Typography>
 
-  const onSubmit = async ({
-    firstName,
-    lastName,
-    username,
-    email,
-    password,
-  }) => {
-    try {
-      const { data } = await executeSignUp({
-        variables: {
-          userInput: {
-            firstName: firstName.toLowerCase().trim(),
-            lastName: lastName.toLowerCase().trim(),
-            username: username.toLowerCase().trim(),
-            email: email.toLowerCase().trim(),
-            password,
-          },
-        },
-      });
-
-      if (data) {
-        navigate("/login", { replace: true });
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const styles = {
-    container: {
-      backgroundColor: "#fff",
-    },
-
-    header: {
-      paddingTop: 2,
-      paddingBottom: 2,
-      backgroundColor: "#F8F8FF",
-      mx: "auto",
-      width: 700,
-    },
-
-    form: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      padding: 4,
-      mx: "auto",
-      width: 700,
-    },
-
-    loadingButton: { marginTop: 3, marginBottom: 2 },
-    errorContainer: {
-      marginTop: 2,
-      color: "#d32f2f",
-      textAlign: "center",
-    },
-  };
-
-  return (
-    <Box sx={styles.container}>
-      <Typography
-        variant="h4"
-        gutterBottom
-        component="h1"
-        align="center"
-        sx={styles.header}
-      >
-        Signup Form
-      </Typography>
-
-      <Typography variant="h6" gutterBottom component="h1" align="center">
-        Create an account
-      </Typography>
-
-      <Divider />
-      <Box component="form" sx={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          margin="normal"
-          id="email"
-          label="Email"
-          name="email"
-          variant="outlined"
-          fullWidth
-          {...register("email", { required: true })}
-          error={!!errors.email}
-          disabled={loading}
-        />
-
-        <TextField
-          margin="normal"
-          id="firstName"
-          label="First Name"
-          name="firstName"
-          variant="outlined"
-          fullWidth
-          {...register("firstName", { required: true })}
-          error={!!errors.firstName}
-          disabled={loading}
-        />
-        <TextField
-          margin="normal"
-          id="lastName"
-          label="Last Name"
-          name="lastName"
-          variant="outlined"
-          fullWidth
-          {...register("lastName", { required: true })}
-          error={!!errors.lastName}
-          disabled={loading}
-        />
-        <TextField
-          margin="normal"
-          id="username"
-          label="Username"
-          name="username"
-          variant="outlined"
-          fullWidth
-          {...register("username", { required: true })}
-          error={!!errors.username}
-          disabled={loading}
-        />
-        <TextField
-          type="password"
-          margin="normal"
-          id="password"
-          label="Password"
-          name="password"
-          variant="outlined"
-          fullWidth
-          {...register("password", { required: true })}
-          error={!!errors.password}
-          disabled={loading}
-        />
-
-        <LoadingButton
-          loading={loading}
-          loadingIndicator="Loading..."
-          variant="contained"
-          fullWidth
-          type="submit"
-          sx={styles.loadingButton}
-          startIcon={error && <ErrorIcon />}
-          color={error ? "error" : "primary"}
-        >
-          Click here to proceed
-        </LoadingButton>
-
-        <Link
-          component={RouterLink}
-          to="/login"
-          variant="body2"
-          underline="none"
-        >
-          Already have an account? Login
-        </Link>
-        {error && (
-          <Typography
-            variant="subtitle2"
-            gutterBottom
-            component="div"
-            sx={styles.errorContainer}
-          >
-            Failed to sign up, please make sure you enter the correct details or
-            try again later.
-          </Typography>
-        )}
-      </Box>
-    </Box>
-  );
+			<Divider />
+			<SignUpForm />
+		</Box>
+	);
 };

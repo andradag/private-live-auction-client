24904f0a04c495300873fb8d4d7ac60222217c33
AkenEagle <akeneagle@gmail.com>
2022-03-09 22:03:48 +0000

    Fixed login/signup issue

--- src/mutations/index.js
+++ src/mutations/index.js
@@ -1,29 +1,31 @@
-import {gql} from "@apollo/client";
+import { gql } from "@apollo/client";
 
 export const LOGIN = gql`
-	mutation Mutation($input: LoginInput!) {
-		login(input: $input) {
-			token
-			user {
-				_id
-				username
-				email
-			}
-		}
-	}
+  mutation Mutation($loginInput: LoginInput!) {
+    login(input: $loginInput) {
+      user {
+        _id
+        username
+        firstName
+        lastName
+        email
+      }
+      token
+    }
+  }
 `;
 
 export const SIGNUP = gql`
-	mutation Mutation($userInput: UserInput!) {
-		addUser(userInput: $userInput) {
-			token
-			user {
-				_id
-				username
-				firstName
-				lastName
-				email
-			}
-		}
-	}
+  mutation Mutation($userInput: UserInput!) {
+    addUser(userInput: $userInput) {
+      token
+      user {
+        _id
+        username
+        firstName
+        lastName
+        email
+      }
+    }
+  }
 `;

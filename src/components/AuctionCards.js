757d0ba32371874dc7754965f3388b87aef80b5d
AkenEagle <akeneagle@gmail.com>
2022-03-10 11:58:27 +0000

    Refactored code duplicates

---
+++ src/components/AuctionCards.js
@@ -0,0 +1,75 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+// Will accept "auction" prop which will inclue title, image etc
+export default function AuctionCards() {
+  const styles = {
+    grid: {
+      paddingTop: 3,
+      paddingBottom: 3,
+    },
+  };
+  // Example auctions
+  const auctions = [
+    {
+      title: "Audi RS5",
+      description: "New 2020 RS5 Black",
+      image:
+        "https://www.quattrodaily.com/wp-content/uploads/2020/06/Audi-RS5-Carbon-Black-1.jpg",
+    },
+    {
+      title: "Juice",
+      description: "Just a glass of Orange Juice",
+      image:
+        "https://www.collinsdictionary.com/images/thumb/fruitjuice_148446767_250.jpg",
+    },
+    {
+      title: "Macbook Pro",
+      description: "New Macbook Pro",
+      image:
+        "https://i.pcmag.com/imagery/reviews/05POeP7aWhKjIKkZ15YCZa9-21..v1635374572.jpg",
+    },
+  ];
+
+  return (
+    <Grid container justifyContent="center" spacing={3} sx={styles.grid}>
+      {auctions.map((auction) => (
+        <Grid key={auction} item>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="150"
+              width="150"
+              // Example of prop usage here would be {auction.image}
+              image={auction.image}
+            />
+            <CardContent>
+              {/* Title */}
+              <Typography gutterBottom variant="h5" component="div">
+                {auction.title}
+              </Typography>
+              {/* Short Description */}
+              <Typography variant="body2" color="text.secondary">
+                {auction.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" variant="outlined">
+                View
+              </Button>
+              <Button size="small" variant="contained">
+                Register
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
--- src/components/pages/Dashboard/index.js
+++ src/components/pages/Dashboard/index.js
@@ -1,11 +1,9 @@
-import AuctionCard from "../../AuctionCard";
+import AuctionCards from "../../AuctionCards";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
-import Grid from "@mui/material/Grid";
 
 export const Dashboard = () => {
-  const auctions = AuctionCard();
   const styles = {
     header: {
       paddingTop: 2,
@@ -13,11 +11,6 @@ export const Dashboard = () => {
       backgroundColor: "#F8F8FF",
       mx: "auto",
       width: 700,
-    },
-
-    grid: {
-      paddingTop: 3,
-      paddingBottom: 3,
     },
   };
   // Query the DB to get the auctions
@@ -35,15 +28,7 @@ export const Dashboard = () => {
           Available Auctions
         </Typography>
         <Divider />
-        {/* AuctionCard will accept props based on data (auction1/2/3) */}
-        {/* Auction data will be mapped accordingly */}
-        <Grid container justifyContent="center" spacing={3} sx={styles.grid}>
-          {auctions.map((value) => (
-            <Grid key={value} item>
-              {value}
-            </Grid>
-          ))}
-        </Grid>
+        <AuctionCards />
       </Box>
     </>
   );

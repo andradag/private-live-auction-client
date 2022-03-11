53a8ed0a02b8600ca3eb1e1213d20ecbb7d297fd
Darion Richards <darionrichards@outlook.com>
2022-03-11 13:01:32 +0000

    Successfully push listing to DB using mutation

--- src/components/BidddingCard.js
+++ src/components/BidddingCard.js
@@ -1,6 +1,6 @@
-import * as React from "react";
-import { Link as RouterLink, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import {useState} from "react";
+import {Link as RouterLink, useNavigate} from "react-router-dom";
+import {useForm} from "react-hook-form";
 
 import Box from "@mui/material/Box";
 import Select from "@mui/material/Select";
@@ -8,175 +8,189 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 
-import { AppBar, TextField, Button } from "@material-ui/core";
-import { CATEGORY, CREATELISTING, LOGIN } from "../mutations";
-import { useMutation, useQuery } from "@apollo/client";
-import { Menu } from "@mui/material";
+import {AppBar, TextField, Button} from "@material-ui/core";
+import {CATEGORY, CREATELISTING, LOGIN} from "../mutations";
+import {useMutation, useQuery} from "@apollo/client";
+import {Menu} from "@mui/material";
 
 // import MenuItem from "@mui/material/MenuItem";
 
 export const BiddingCard = () => {
-  const [categoryId, setCategoryId] = React.useState();
-  const { loading, error, data } = useQuery(CATEGORY);
-  const [
-    executeCreateListing,
-    { loading: loadingListing, error: errorListing },
-  ] = useMutation(CREATELISTING);
-  console.log(data);
+	const [categoryId, setCategoryId] = useState();
+	const {loading, error, data} = useQuery(CATEGORY);
+	const [executeCreateListing, {loading: loadingListing, error: errorListing}] =
+		useMutation(CREATELISTING);
+	console.log(data);
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const getTarget = (event) => {
-    console.log(event.currentTarget.id);
-    setCategoryId(event.currentTarget.id);
-  };
+	const {
+		register,
+		handleSubmit,
+		formState: {errors},
+	} = useForm();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+	const onSubmit = async (auction) => {
+		const {data} = await executeCreateListing({
+			variables: {
+				listingInput: {
+					title: auction.title,
+					description: auction.description,
+					category: categoryId,
+					reserveAmount: parseFloat(auction.reserveAmount),
+					startingBid: parseFloat(auction.startingBid),
+				},
+			},
+		});
 
-  const onSubmit = async (auction) => {
-    const { data } = await executeCreateListing({
-      variables: {
-        listingInput: {
-          title: auction.title,
-          description: auction.description,
-          category: categoryId,
-          reserveAmount: auction.reserveAmount,
-          startingBid: auction.startingBid,
-        },
-      },
-    });
+		console.log(auction);
 
-    console.log(auction);
+		// if (data) {
+		//   const { token, user } = data.login;
 
-    // if (data) {
-    //   const { token, user } = data.login;
+		//   localStorage.setItem("token", token);
+		//   localStorage.setItem("user", JSON.stringify(user));
 
-    //   localStorage.setItem("token", token);
-    //   localStorage.setItem("user", JSON.stringify(user));
+		//   setIsLoggedIn(true);
+		//   setUser({
+		//     id: user.id,
+		//     firstName: user.firstName,
+		//     lastName: user.lastName,
+		//     email: user.email,
+		//     username: user.username,
+		//   });
 
-    //   setIsLoggedIn(true);
-    //   setUser({
-    //     id: user.id,
-    //     firstName: user.firstName,
-    //     lastName: user.lastName,
-    //     email: user.email,
-    //     username: user.username,
-    //   });
+		//   navigate("/dashboard", { replace: true });
+		// }
+	};
 
-    //   navigate("/dashboard", { replace: true });
-    // }
-  };
+	const handleChange = (event) => {
+		console.log(event.target.value);
+		setCategoryId(event.target.value);
+	};
 
-  const handleChange = (event) => {
-    // option(event.target.value);
-  };
+	const styles = {
+		form: {
+			display: "flex",
+			flexDirection: "column",
+			alignItems: "center",
+			padding: 4,
+			mx: "auto",
+			width: 700,
+		},
+	};
 
-  const styles = {
-    form: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      padding: 4,
-      mx: "auto",
-      width: 700,
-    },
-  };
+	return (
+		<Box component="form" sx={styles.form} onSubmit={handleSubmit(onSubmit)}>
+			<TextField
+				margin="normal"
+				id="title"
+				label="Title"
+				name="title"
+				variant="outlined"
+				fullWidth
+				{...register("title", {required: true})}
+				error={!!errors.title}
+				// disabled={loading}
+			/>
 
-  return (
-    <Box component="form" sx={styles.form} onSubmit={handleSubmit(onSubmit)}>
-      <TextField
-        margin="normal"
-        id="title"
-        label="Title"
-        name="title"
-        variant="outlined"
-        fullWidth
-        {...register("title", { required: true })}
-        error={!!errors.title}
-        // disabled={loading}
-      />
+			<br />
 
-      <br />
+			<TextField
+				margin="normal"
+				id="description"
+				label="Description"
+				name="description"
+				variant="outlined"
+				fullWidth
+				{...register("description", {required: true})}
+				error={!!errors.description}
+				// disabled={loading}
+			/>
 
-      <TextField
-        margin="normal"
-        id="description"
-        label="Description"
-        name="description"
-        variant="outlined"
-        fullWidth
-        {...register("description", { required: true })}
-        error={!!errors.description}
-        // disabled={loading}
-      />
+			<br />
 
-      <br />
+			{/* <FormControl fullWidth>
+				<InputLabel id="title">Category</InputLabel>
+				<Select
+					labelId="category"
+					id="category"
+					label="category"
+					{...register("category")}
+					defaultValue=""
+					autoFocus
+					// disabled={loading}
+				>
+					{data &&
+						data.getAllCategories.map((category, index) => {
+							return (
+								<MenuItem
+									onClick={getTarget}
+									key={index}
+									value={category._id}
+									id={category._id}
+								>
+									{category.title}
+								</MenuItem>
+							);
+						})}
+				</Select>
+				{errors.title && "Please select your title"}
+			</FormControl> */}
 
-      <FormControl fullWidth>
-        <InputLabel id="title">Category</InputLabel>
-        <Select
-          labelId="category"
-          id="category"
-          label="category"
-          {...register("category")}
-          defaultValue=""
-          autoFocus
-          // disabled={loading}
-        >
-          {data &&
-            data.getAllCategories.map((category, index) => {
-              return (
-                <MenuItem
-                  onClick={getTarget}
-                  key={index}
-                  value={category._id}
-                  id={category._id}
-                >
-                  {category.title}
-                </MenuItem>
-              );
-            })}{" "}
-        </Select>
-        {errors.title && "Please select your title"}
-      </FormControl>
+			<FormControl fullWidth>
+				<InputLabel id="category-select">Category</InputLabel>
+				<Select
+					labelId="category-select"
+					id="category"
+					defaultValue=""
+					label="Category"
+					onChange={handleChange}
+				>
+					{data &&
+						data.getAllCategories.map((category, index) => {
+							return (
+								<MenuItem key={index} value={category._id}>
+									{category.title}
+								</MenuItem>
+							);
+						})}
+				</Select>
+			</FormControl>
 
-      <br />
+			<br />
 
-      <TextField
-        margin="normal"
-        id="reserveAmount"
-        type="number"
-        label="Reserve Amount"
-        name="reserveAmount"
-        variant="outlined"
-        fullWidth
-        {...register("reserveAmount", { required: true })}
-        error={!!errors.reserveAmount}
-        // disabled={loading}
-      />
+			<TextField
+				margin="normal"
+				id="reserveAmount"
+				type="number"
+				label="Reserve Amount"
+				name="reserveAmount"
+				variant="outlined"
+				fullWidth
+				{...register("reserveAmount", {required: true})}
+				error={!!errors.reserveAmount}
+				// disabled={loading}
+			/>
 
-      <br />
+			<br />
 
-      <TextField
-        margin="normal"
-        id="startingBid"
-        type="number"
-        label="Starting Bid"
-        name="startingBid"
-        variant="outlined"
-        fullWidth
-        {...register("startingBid", { required: true })}
-        error={!!errors.startingBid}
-        // disabled={loading}
-      />
+			<TextField
+				margin="normal"
+				id="startingBid"
+				type="number"
+				label="Starting Bid"
+				name="startingBid"
+				variant="outlined"
+				fullWidth
+				{...register("startingBid", {required: true})}
+				error={!!errors.startingBid}
+				// disabled={loading}
+			/>
 
-      <br />
+			<br />
 
-      {/* <input
+			{/* <input
         accept="image/*"
         style={{ display: "none" }}
         id="raised-button-file"
@@ -189,11 +203,11 @@ export const BiddingCard = () => {
         </Button>
       </label> */}
 
-      <br />
+			<br />
 
-      <Button variant="contained" type="submit" color="primary">
-        Save and continue
-      </Button>
-    </Box>
-  );
+			<Button variant="contained" type="submit" color="primary">
+				Save and continue
+			</Button>
+		</Box>
+	);
 };

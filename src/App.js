6c6df2b4591db61a36dccd4a580c8d8e3a507122
Asmaa <Asmaa99musse@gmail.com>
2022-03-14 22:12:55 +0000

    Resoved conflict

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import { AppProvider } from "./contexts/AppProvider";
 import { Navbar } from "./components/Navbar";
 
 import { AppRoutes } from "./AppRoutes.js";
-import { Login } from "./components/pages/Login";
-import { SignUp } from "./components/pages/SignUp";
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GRAPHQL_API || "http://localhost:4000/graphql",

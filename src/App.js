72e10a1ee1dda322ddf8c4cc2ab0ed7f4bee6342
Asmaa <Asmaa99musse@gmail.com>
2022-03-14 21:57:29 +0000

    Added card styling

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import {
-	ApolloClient,
-	InMemoryCache,
-	ApolloProvider,
-	createHttpLink,
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
 } from "@apollo/client";
-import {setContext} from "@apollo/client/link/context";
-import {AppProvider} from "./contexts/AppProvider";
+import { setContext } from "@apollo/client/link/context";
+import { AppProvider } from "./contexts/AppProvider";
 
-import {Navbar} from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
 
-import {AppRoutes} from "./AppRoutes.js";
+import { AppRoutes } from "./AppRoutes.js";
+import { Login } from "./components/pages/Login";
 
 const httpLink = createHttpLink({
-	uri: process.env.REACT_APP_GRAPHQL_API || "http://localhost:4000/graphql",
+  uri: process.env.REACT_APP_GRAPHQL_API || "http://localhost:4000/graphql",
 });
 
-const authLink = setContext((_, {headers}) => {
-	const token = localStorage.getItem("token");
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
 
-	return {
-		headers: {
-			...headers,
-			authorization: token ? `Bearer ${token}` : "",
-		},
-	};
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-	link: authLink.concat(httpLink),
-	cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 export const App = () => {
-	return (
-		<ApolloProvider client={client}>
-			<AppProvider>
-				<BrowserRouter>
-					<Navbar />
-					<AppRoutes />
-				</BrowserRouter>
-			</AppProvider>
-		</ApolloProvider>
-	);
+  return (
+    <ApolloProvider client={client}>
+      <AppProvider>
+        <BrowserRouter>
+          <Login />
+          {/* <Navbar />
+          <AppRoutes /> */}
+        </BrowserRouter>
+      </AppProvider>
+    </ApolloProvider>
+  );
 };

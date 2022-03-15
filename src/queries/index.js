c26c274e978191bc99f7c0755071dd40c7906145
Darion Richards <darionrichards@outlook.com>
2022-03-15 19:58:34 +0000

    Fixed query errors

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,51 +1,55 @@
-import { gql } from "@apollo/client";
+import {gql} from "@apollo/client";
 
 export const GET_USER = gql`
-  query Query($userId: ID!) {
-    getSingleUser(userId: $userId) {
-      _id
-      username
-      firstName
-      lastName
-      email
-      isAdmin
-      savedListings {
-        _id
-        title
-        description
-        category
-        startingBid
-        reserveAmount
-        status
-      }
-    }
-  }
+	query Query($userId: ID!) {
+		getSingleUser(userId: $userId) {
+			_id
+			username
+			firstName
+			lastName
+			email
+			isAdmin
+			savedListings {
+				_id
+				title
+				description
+				category {
+					title
+				}
+				startingBid
+				reserveAmount
+				status
+			}
+		}
+	}
 `;
 
 export const GET_LISTINGS = gql`
-  query Query($status: String, $category: ID) {
-    getListings(status: $status, category: $category) {
-      _id
-      title
-      description
-      category
-      reserveAmount
-      startingBid
-      status
-    }
-  }
+	query Query($status: String, $category: ID) {
+		getListings(status: $status, category: $category) {
+			_id
+			title
+			description
+			category {
+				title
+			}
+			reserveAmount
+			startingBid
+			status
+		}
+	}
 `;
 
 export const GET_SINGLE_LISTING = gql`
-  query GetSingleListing($id: ID!) {
-    getSingleListing(_id: $id) {
-      _id
-      title
-      description
-      reserveAmount
-      category
-      startingBid
-      status
-    }
-  }
+	query GetSingleListing($id: ID!) {
+		getSingleListing(_id: $id) {
+			_id
+			title
+			description
+			reserveAmount
+			category
+			startingBid
+			status
+		}
+	}
 `;

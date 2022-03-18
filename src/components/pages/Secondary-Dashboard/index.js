2a090a1705a4885b3ea84ccf07b97070a2b19084
Asmaa <Asmaa99musse@gmail.com>
2022-03-18 21:04:56 +0000

    completed styling for right hand side card

--- src/components/pages/Secondary-Dashboard/index.js
+++ src/components/pages/Secondary-Dashboard/index.js
@@ -78,14 +78,13 @@ export const SecondaryDashboardPage = () => {
         <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
           <Grid item xs={6}>
             <Item>
-              <Card sx={{ marginTop: 20, width: 400, height: 645 }}>
+              <Card sx={{ marginTop: 20, width: 500, height: 645 }}>
                 <CardMedia
                   component="img"
                   height="50%"
                   image="https://loveincorporated.blob.core.windows.net/contentimages/gallery/39d5432f-42ac-4851-9003-0233243ecc15-Karl_L_Monaco5.jpg"
                 />
                 <CardContent sx={{ height: 80, marginBottom: 20 }}>
-                  {/* Title */}
                   <h3>French Riviera Summer Home </h3>
                   <Typography
                     gutterBottom
@@ -93,7 +92,7 @@ export const SecondaryDashboardPage = () => {
                     component="div"
                     textAlign="justify"
                   ></Typography>
-                  {/* Short Description */}
+
                   <h5>
                     Built in 1902, the Belle Epoque style of the property makes
                     for particularly impressive environs, with gorgeous wall
@@ -129,21 +128,20 @@ export const SecondaryDashboardPage = () => {
                 </CardActions>
               </Card>
 
-              <Card sx={{ marginTop: 10, width: 345, height: 645 }}>
+              <Card sx={{ marginTop: 10, width: 500, height: 645 }}>
                 <CardMedia
                   component="img"
                   height="50%"
                   image="https://static2.mansionglobal.com/production/media/article-images/fd7410aea10cf82c1b5a8b774168fa3e/large_Villa-Acqualina.jpg"
                 />
                 <CardContent sx={{ height: 80, marginBottom: 20 }}>
-                  {/* Title */}
                   <h3>Acqualina Estate, Sunny Isles Beach</h3>
                   <Typography
                     gutterBottom
                     variant="h5"
                     component="div"
                   ></Typography>
-                  {/* Short Description */}
+
                   <h5>
                     Acqualina Estate consists of two 50-story towers with 265
                     residences, ranging from three- to seven-bedrooms. Prices
@@ -179,21 +177,20 @@ export const SecondaryDashboardPage = () => {
                 </CardActions>
               </Card>
 
-              <Card sx={{ marginTop: 10, width: 345, height: 645 }}>
+              <Card sx={{ marginTop: 10, width: 500, height: 645 }}>
                 <CardMedia
                   component="img"
                   height="50%"
                   image="https://media.architecturaldigest.com/photos/5b3264390dcdbf2512f61c9e/3:2/w_3543,h_2362,c_limit/Villa%20Jako(c)%20Engel%20_%20Vo%CC%88lkers_Herbert%20Ohge%20(1).jpg"
                 />
                 <CardContent sx={{ height: 80, marginBottom: 20 }}>
-                  {/* Title */}
                   <h3>Villa Jako, Elbe River</h3>
                   <Typography
                     gutterBottom
                     variant="h5"
                     component="div"
                   ></Typography>
-                  {/* Short Description */}
+
                   <h5>
                     Engel & Völkers in Hamburg has listed the classical home,
                     which features a Roman water pool in the grant foyer, an
@@ -228,21 +225,20 @@ export const SecondaryDashboardPage = () => {
                 </CardActions>
               </Card>
 
-              <Card sx={{ marginTop: 10, width: 345, height: 645 }}>
+              <Card sx={{ marginTop: 10, width: 500, height: 645 }}>
                 <CardMedia
                   component="img"
                   height="50%"
                   image="https://www.theoneatelier.com/wp-content/uploads/2021/11/Karl-Lagerfeld-Sierra-Blanca-Estates_0.jpg"
                 />
                 <CardContent sx={{ height: 80, marginBottom: 40 }}>
-                  {/* Title */}
                   <h3>KARL LAGERFELD Villas Marbella</h3>
                   <Typography
                     gutterBottom
                     variant="h5"
                     component="div"
                   ></Typography>
-                  {/* Short Description */}
+
                   <h5>
                     The general landscape that dominates the development acts as
                     a sort of “lung” to the masterplan while the construction of
@@ -279,21 +275,20 @@ export const SecondaryDashboardPage = () => {
                   </Button>
                 </CardActions>
               </Card>
-              <Card sx={{ marginTop: 10, width: 345, height: 645 }}>
+              <Card sx={{ marginTop: 10, width: 500, height: 645 }}>
                 <CardMedia
                   component="img"
                   height="50%"
                   image="https://content.fortune.com/wp-content/uploads/2016/07/east-to-west-all-4-towers-east-to-west.jpg?resize=1500,1000"
                 />
                 <CardContent sx={{ height: 80, marginBottom: 20 }}>
-                  {/* Title */}
                   <h3>Residential Real Estate, South Florida</h3>
                   <Typography
                     gutterBottom
                     variant="h5"
                     component="div"
                   ></Typography>
-                  {/* Short Description */}
+
                   <h5>
                     The Miami-based Trump Group attached Karl Lagerfeld'
                     name—the head designer and creative director of Chanel and
@@ -324,21 +319,20 @@ export const SecondaryDashboardPage = () => {
                   </Button>
                 </CardActions>
               </Card>
-              <Card sx={{ marginTop: 10, width: 345, height: 645 }}>
+              <Card sx={{ marginTop: 10, width: 500, height: 645 }}>
                 <CardMedia
                   component="img"
                   height="50%"
                   image="https://scontent-man2-1.xx.fbcdn.net/v/t1.6435-9/90197364_3182024548495558_1605461090232172544_n.jpg?stp=cp0_dst-jpg_e15_p320x320_q65&_nc_cat=105&ccb=1-5&_nc_sid=2d5d41&_nc_ohc=9A4P2Mx9K2kAX_lK4uc&_nc_oc=AQkIBWQ7Xisgpye7khc3F2KwBL4rTeCEwAHgGm7MTFEiVErXzvGoKYjSvQKnTrZUW3uIZPlQEp2_-mAkgLkoH1Nq&_nc_ht=scontent-man2-1.xx&oh=00_AT8DMaxCvZebs1TnLVPmshLnfwpTeKVZJD5gt77SdlHHhw&oe=62596A73"
                 />
                 <CardContent sx={{ height: 80, marginBottom: 20 }}>
-                  {/* Title */}
                   <h3>21 rue St-Guillaume, Paris</h3>
                   <Typography
                     gutterBottom
                     variant="h5"
                     component="div"
                   ></Typography>
-                  {/* Short Description */}
+
                   <h5>
                     The historic building is considered the heart and soul of
                     the brand; it housed Karl Lagerfeld’s office and studio,
@@ -368,21 +362,20 @@ export const SecondaryDashboardPage = () => {
                   </Button>
                 </CardActions>
               </Card>
-              <Card sx={{ marginTop: 10, width: 345, height: 645 }}>
+              <Card sx={{ marginTop: 10, width: 500, height: 645 }}>
                 <CardMedia
                   component="img"
                   height="50%"
                   image="https://media.vogue.co.uk/photos/61a7a6fdcfac31880007b3ce/2:3/w_2240,c_limit/PF2176_07-2008%20KL%20VOLTAIRE-044Bleucouve_comp.jpg"
                 />
                 <CardContent sx={{ height: 80, marginBottom: 20 }}>
-                  {/* Title */}
                   <h3>Quai Voltaire apartment</h3>
                   <Typography
                     gutterBottom
                     variant="h5"
                     component="div"
                   ></Typography>
-                  {/* Short Description */}
+
                   <h5>
                     The apartment, which Lagerfeld spent two and a half years
                     renovating before moving in in 2007, clusters of unopened
@@ -426,10 +419,11 @@ export const SecondaryDashboardPage = () => {
                 container
                 rowSpacing={1}
                 columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+                sx={{ margin: 20, justifyContent: "center" }}
               >
                 <Grid>
                   <Item>
-                    <CardContent sx={{ marginTop: 10 }}>
+                    <CardContent>
                       <h1>French Riviera Summer Home </h1>
                       <Typography
                         gutterBottom
@@ -810,10 +804,9 @@ export const SecondaryDashboardPage = () => {
             image="https://static2.mansionglobal.com/production/media/article-images/fd7410aea10cf82c1b5a8b774168fa3e/large_Villa-Acqualina.jpg"
           />
           <CardContent sx={{ height: 80, marginBottom: 20 }}>
-            {/* Title */}
             <h3>Acqualina Estate, Sunny Isles Beach</h3>
             <Typography gutterBottom variant="h5" component="div"></Typography>
-            {/* Short Description */}
+
             <h5>
               Acqualina Estate consists of two 50-story towers with 265
               residences, ranging from three- to seven-bedrooms. Prices start at
@@ -852,10 +845,9 @@ export const SecondaryDashboardPage = () => {
             image="https://media.architecturaldigest.com/photos/5b3264390dcdbf2512f61c9e/3:2/w_3543,h_2362,c_limit/Villa%20Jako(c)%20Engel%20_%20Vo%CC%88lkers_Herbert%20Ohge%20(1).jpg"
           />
           <CardContent sx={{ height: 80, marginBottom: 20 }}>
-            {/* Title */}
             <h3>Villa Jako, Elbe River</h3>
             <Typography gutterBottom variant="h5" component="div"></Typography>
-            {/* Short Description */}
+
             <h5>
               Engel & Völkers in Hamburg has listed the classical home, which
               features a Roman water pool in the grant foyer, an atrium and
@@ -894,10 +886,9 @@ export const SecondaryDashboardPage = () => {
             image="https://www.theoneatelier.com/wp-content/uploads/2021/11/Karl-Lagerfeld-Sierra-Blanca-Estates_0.jpg"
           />
           <CardContent sx={{ height: 80, marginBottom: 40 }}>
-            {/* Title */}
             <h3>KARL LAGERFELD Villas Marbella</h3>
             <Typography gutterBottom variant="h5" component="div"></Typography>
-            {/* Short Description */}
+
             <h5>
               The general landscape that dominates the development acts as a
               sort of “lung” to the masterplan while the construction of the
@@ -939,10 +930,9 @@ export const SecondaryDashboardPage = () => {
             image="https://content.fortune.com/wp-content/uploads/2016/07/east-to-west-all-4-towers-east-to-west.jpg?resize=1500,1000"
           />
           <CardContent sx={{ height: 80, marginBottom: 20 }}>
-            {/* Title */}
             <h3>Residential Real Estate, South Florida</h3>
             <Typography gutterBottom variant="h5" component="div"></Typography>
-            {/* Short Description */}
+
             <h5>
               The Miami-based Trump Group attached Karl Lagerfeld' name—the head
               designer and creative director of Chanel and Fendi— to their new
@@ -978,10 +968,9 @@ export const SecondaryDashboardPage = () => {
             image="https://scontent-man2-1.xx.fbcdn.net/v/t1.6435-9/90197364_3182024548495558_1605461090232172544_n.jpg?stp=cp0_dst-jpg_e15_p320x320_q65&_nc_cat=105&ccb=1-5&_nc_sid=2d5d41&_nc_ohc=9A4P2Mx9K2kAX_lK4uc&_nc_oc=AQkIBWQ7Xisgpye7khc3F2KwBL4rTeCEwAHgGm7MTFEiVErXzvGoKYjSvQKnTrZUW3uIZPlQEp2_-mAkgLkoH1Nq&_nc_ht=scontent-man2-1.xx&oh=00_AT8DMaxCvZebs1TnLVPmshLnfwpTeKVZJD5gt77SdlHHhw&oe=62596A73"
           />
           <CardContent sx={{ height: 80, marginBottom: 20 }}>
-            {/* Title */}
             <h3>21 rue St-Guillaume, Paris</h3>
             <Typography gutterBottom variant="h5" component="div"></Typography>
-            {/* Short Description */}
+
             <h5>
               The historic building is considered the heart and soul of the
               brand; it housed Karl Lagerfeld’s office and studio, where he
@@ -1015,10 +1004,9 @@ export const SecondaryDashboardPage = () => {
             image="https://media.vogue.co.uk/photos/61a7a6fdcfac31880007b3ce/2:3/w_2240,c_limit/PF2176_07-2008%20KL%20VOLTAIRE-044Bleucouve_comp.jpg"
           />
           <CardContent sx={{ height: 80, marginBottom: 20 }}>
-            {/* Title */}
             <h3>Quai Voltaire apartment</h3>
             <Typography gutterBottom variant="h5" component="div"></Typography>
-            {/* Short Description */}
+
             <h5>
               The apartment, which Lagerfeld spent two and a half years
               renovating before moving in in 2007, clusters of unopened pink and

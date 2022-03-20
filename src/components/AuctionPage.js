e9d45e321760721ccde650a45530730b8ebca793
Darion Richards <darionrichards@outlook.com>
2022-03-20 16:09:21 +0000

    Further refactored

--- src/components/AuctionPage.js
+++ src/components/AuctionPage.js
@@ -4,26 +4,20 @@ import {useParams} from "react-router-dom";
 import {useQuery} from "@apollo/client";
 import {useSubscription} from "@apollo/client";
 
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import CardActionArea from "@mui/material/CardActionArea";
 import Divider from "@mui/material/Divider";
-import CardActions from "@mui/material/CardActions";
-import Button from "@mui/material/Button";
 
 import {GET_SINGLE_LISTING} from "../queries";
 import {AUCTION_BID_SUBSCRIPTION} from "../subscriptions";
-import {PostBidModal} from "./PostBidModal";
+
 import {BiddingCard} from "./BiddingCard";
+import {ListingItem} from "./ListingItem";
 
 export const AuctionPage = () => {
 	const {id} = useParams();
 	const [currentBid, setCurrentBid] = useState({});
 	const [auctionData, setAuctionData] = useState([]);
-	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const {data, error, loading} = useQuery(GET_SINGLE_LISTING, {
 		variables: {id},
@@ -48,43 +42,14 @@ export const AuctionPage = () => {
 		}
 	}, [subscriptionData, data]);
 
-	const handleModalOpen = () => setIsModalOpen(true);
-	const handleModalClose = () => setIsModalOpen(false);
-
 	const styles = {
-		listingContainer: {
-			display: "flex",
-			flexWrap: "wrap",
-			padding: 3,
-			margin: 1,
-			mx: "auto",
-			width: "75%",
-		},
-		listingCard: {
-			display: "flex",
-			flexDirection: "column",
-			justifyContent: "space-evenly",
-			alignItems: "center",
-			boxShadow: 4,
-			width: "50%",
-		},
-		listingCardArea: {
-			height: 1,
-		},
-		listingCardContent: {},
-
-		listingCardAction: {
-			justifyContent: "center",
-			width: 1,
-		},
-		listingButtons: {
-			display: "flex",
-			flexDirection: "column",
-			width: "40%",
-			height: "50px",
-		},
 		divider: {
 			borderColor: "rgb(0 0 0 / 34%)",
+		},
+		biddingTitle: {
+			textAlign: "center",
+			textDecoration: "underline",
+			margin: 2,
 		},
 		biddingContainer: {
 			width: "100%",
@@ -93,11 +58,6 @@ export const AuctionPage = () => {
 			alignItems: "center",
 			gap: "1rem",
 		},
-		biddingTitle: {
-			textAlign: "center",
-			textDecoration: "underline",
-			margin: 2,
-		},
 	};
 
 	if (error || loading) return <h1>Error</h1>;
@@ -105,58 +65,7 @@ export const AuctionPage = () => {
 	return (
 		data?.getSingleListing && (
 			<>
-				<Box sx={styles.listingContainer}>
-					<Card sx={styles.listingCard}>
-						<CardActionArea sx={styles.listingCardArea}>
-							<CardMedia
-								component="img"
-								height="200"
-								image="https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
-								alt="Macbook Air"
-							/>
-							<CardContent sx={styles.listingCardContent}>
-								<Typography gutterBottom variant="h5" component="div">
-									{data.getSingleListing.title}
-								</Typography>
-								<Typography gutterBottom variant="h7" component="div">
-									{data.getSingleListing.category.title}
-								</Typography>
-								<Typography variant="body2" color="text.secondary">
-									{data.getSingleListing.description}
-								</Typography>
-							</CardContent>
-						</CardActionArea>
-					</Card>
-					<Card sx={styles.listingCard}>
-						<CardContent sx={{p: 0}}>
-							{currentBid && (
-								<Typography gutterBottom variant="h4" component="div">
-									Current bid: £{currentBid?.amount}
-								</Typography>
-							)}
-						</CardContent>
-						<CardActions sx={styles.listingCardAction}>
-							<Button sx={styles.listingButtons} variant="contained">
-								BID £550
-							</Button>
-
-							<Button
-								sx={styles.listingButtons}
-								variant="contained"
-								onClick={handleModalOpen}
-							>
-								CUSTOM BID
-							</Button>
-							<PostBidModal
-								open={isModalOpen}
-								onClose={handleModalClose}
-								listingId={id}
-								currentBid={currentBid}
-								startingBid={data.getSingleListing.startingBid}
-							/>
-						</CardActions>
-					</Card>
-				</Box>
+				<ListingItem listingId={id} data={data} currentBid={currentBid} />
 				<Divider sx={styles.divider} />
 				<Typography
 					gutterBottom

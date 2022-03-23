71ef2bdd57ab5bf4208de05a13cbceadb115cf4f
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-03-23 23:21:11 +0000

    added carousel to home page

---
+++ src/components/pages/Home/Carousel.js
@@ -0,0 +1,124 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import ButtonBase from "@mui/material/ButtonBase";
+import Typography from "@mui/material/Typography";
+
+const images = [
+  {
+    url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-karl-lagerfeld-home-hamburg-germany-index2-1530037821.jpg?crop=1.00xw:1.00xh;0,0&resize=980:*",
+    title: "Villa Jako, Germany",
+    width: "40%",
+  },
+  {
+    url: "https://www.monaco-tribune.com/wp-content/uploads/2021/01/karl-lagerfeld-villa-monaco-1024x569.jpg",
+    title: "Pearl White, Monaco",
+    width: "30%",
+  },
+  {
+    url: "https://media.admagazine.fr/photos/60c75d2fd9ccb5da855d6d9c/master/w_2240%2Cc_limit/07_2008_kl_voltaire_044bleucouve_jpg_5994_north_655x492_transparent_jpg_7630.jpg",
+    title: "Quai Voltaire, Paris",
+    width: "30%",
+  },
+];
+
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: "relative",
+  height: 200,
+  [theme.breakpoints.down("sm")]: {
+    width: "100% !important", // Overrides inline-style
+    height: 100,
+  },
+  "&:hover, &.Mui-focusVisible": {
+    zIndex: 1,
+    "& .MuiImageBackdrop-root": {
+      opacity: 0.15,
+    },
+    "& .MuiImageMarked-root": {
+      opacity: 0,
+    },
+    "& .MuiTypography-root": {
+      border: "4px solid currentColor",
+    },
+  },
+}));
+
+const ImageSrc = styled("span")({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: "cover",
+  backgroundPosition: "center 40%",
+});
+
+const Image = styled("span")(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled("span")(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create("opacity"),
+}));
+
+const ImageMarked = styled("span")(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: "absolute",
+  bottom: -2,
+  left: "calc(50% - 9px)",
+  transition: theme.transitions.create("opacity"),
+}));
+
+export default function ButtonBases() {
+  return (
+    <Box
+      sx={{ display: "flex", flexWrap: "wrap", minWidth: 300, width: "100%" }}
+    >
+      {images.map((image) => (
+        <ImageButton
+          focusRipple
+          key={image.title}
+          style={{
+            width: image.width,
+          }}
+        >
+          <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+          <ImageBackdrop className="MuiImageBackdrop-root" />
+          <Image>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              sx={{
+                position: "relative",
+                p: 4,
+                pt: 2,
+                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+              }}
+            >
+              {image.title}
+              <ImageMarked className="MuiImageMarked-root" />
+            </Typography>
+          </Image>
+        </ImageButton>
+      ))}
+    </Box>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,28 @@
+import Carousel from "../../components/pages/Home/Carousel";
+import Footer from "../../components/pages/Home/Footer";
+import Grid from "@mui/material/Grid";
+
 export const Home = () => {
-	// Put auction cards here as well
-	return <h1>Home Page</h1>;
+  // Put auction cards here as well
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Carousel />
+      <Grid>
+		<h2>Established in 1744, Sotheby’s is the world’s largest, most trusted and
+        dynamic marketplace for art and luxury. We empower our international
+        community of collectors and connoisseurs to discover, acquire, finance
+        and consign fine art and rare objects. Our reputation for trust and
+        authenticity is backed by our unparalleled global network of specialists
+        spanning 40 countries and 44 departments, which include Contemporary
+        Art, Modern and Impressionist Art, Old Masters, Chinese Works of Art,
+        jewelry, watches, wine and spirits, and interiors, among many others.
+        Guided by our forward-thinking spirit of innovation, we host over 600
+        auctions annually and offer a cross-category selection of items
+        available for immediate purchase via both digital and physical shopping
+        experiences as well as private sales. </h2>
+      <Grid />
+      <Footer />
+	  </>
+  );
 };

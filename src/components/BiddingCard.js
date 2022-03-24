c94e4f26248fc60232792491615371deaf335c44
surajverma2587 <surajverma2587@gmail.com>
2022-03-24 19:20:30 +0000

    wip

--- src/components/BiddingCard.js
+++ src/components/BiddingCard.js
@@ -4,50 +4,54 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
 import Typography from "@mui/material/Typography";
 
-import {useAuth} from "../contexts/AppProvider";
+import { useAuth } from "../contexts/AppProvider";
 
-export const BiddingCard = ({bid}) => {
-	const {user} = useAuth();
+export const BiddingCard = ({ bid }) => {
+  const { user } = useAuth();
 
-	return (
-		<>
-			<ListItem
-				sx={{
-					justifyItems: "center",
-					alignItems: "center",
-					p: 2.5,
-				}}
-				disablePadding
-				divider
-				secondaryAction={<Typography>{bid.bidTime}</Typography>}
-			>
-				<ListItemAvatar>
-					<Avatar alt={user.username} src={user.imageUrl} />
-				</ListItemAvatar>
-				<ListItemText
-					primary={
-						<>
-							<Typography
-								sx={{display: "inline"}}
-								component="span"
-								variant="h6"
-							>
-								{bid.user.firstName} {bid.user.lastName}
-							</Typography>
-							<Typography
-								sx={{display: "inline"}}
-								component="span"
-								variant="body1"
-							>
-								{` (${bid.user.username})`}
-							</Typography>
-						</>
-					}
-					secondary={
-						<Typography variant="body1">Placed bid: £{bid.amount}</Typography>
-					}
-				/>
-			</ListItem>
-		</>
-	);
+  if (!bid) {
+    return null;
+  }
+
+  return (
+    <>
+      <ListItem
+        sx={{
+          justifyItems: "center",
+          alignItems: "center",
+          p: 2.5,
+        }}
+        disablePadding
+        divider
+        secondaryAction={<Typography>{bid.bidTime}</Typography>}
+      >
+        <ListItemAvatar>
+          <Avatar alt={user.username} src={user.imageUrl} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={
+            <>
+              <Typography
+                sx={{ display: "inline" }}
+                component="span"
+                variant="h6"
+              >
+                {bid.user.firstName} {bid.user.lastName}
+              </Typography>
+              <Typography
+                sx={{ display: "inline" }}
+                component="span"
+                variant="body1"
+              >
+                {` (${bid.user.username})`}
+              </Typography>
+            </>
+          }
+          secondary={
+            <Typography variant="body1">Placed bid: £{bid.amount}</Typography>
+          }
+        />
+      </ListItem>
+    </>
+  );
 };
--- src/pages/AuctionPage/index.js
+++ src/pages/AuctionPage/index.js
@@ -1,101 +1,106 @@
-import {useState, useEffect} from "react";
-import {useParams} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-import {useQuery} from "@apollo/client";
-import {useSubscription} from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { useSubscription } from "@apollo/client";
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 
-import {GET_SINGLE_LISTING} from "../../queries";
-import {AUCTION_BID_SUBSCRIPTION} from "../../subscriptions";
+import { GET_SINGLE_LISTING } from "../../queries";
+import { AUCTION_BID_SUBSCRIPTION } from "../../subscriptions";
 
-import {BiddingCard} from "../../components/BiddingCard";
-import {ListingItem} from "../../components/ListingItem";
+import { BiddingCard } from "../../components/BiddingCard";
+import { ListingItem } from "../../components/ListingItem";
 
 export const AuctionPage = () => {
-	const {id} = useParams();
-	const [currentBid, setCurrentBid] = useState({});
-	const [status, setStatus] = useState("Upcoming");
-	const [auctionData, setAuctionData] = useState([]);
+  const { id } = useParams();
+  const [currentBid, setCurrentBid] = useState({});
+  const [status, setStatus] = useState("Upcoming");
+  const [auctionData, setAuctionData] = useState([]);
 
-	const {data, error, loading} = useQuery(GET_SINGLE_LISTING, {
-		variables: {id},
-	});
+  const { data, error, loading } = useQuery(GET_SINGLE_LISTING, {
+    variables: { id },
+  });
 
-	const {data: subscriptionData, loading: subscriptionLoading} =
-		useSubscription(AUCTION_BID_SUBSCRIPTION, {
-			variables: {
-				listingId: id,
-			},
-		});
+  const { data: subscriptionData, loading: subscriptionLoading } =
+    useSubscription(AUCTION_BID_SUBSCRIPTION, {
+      variables: {
+        listingId: id,
+      },
+    });
 
-	useEffect(() => {
-		if (data) {
-			setAuctionData(data?.getSingleListing?.bids);
-			setCurrentBid(data.getSingleListing.currentBid);
-		}
+  useEffect(() => {
+    if (data) {
+      setAuctionData(data?.getSingleListing?.bids);
+      setCurrentBid(data?.getSingleListing?.currentBid);
+      setStatus(data?.getSingleListing?.status);
+    }
 
-		if (subscriptionData) {
-			setAuctionData([...auctionData, subscriptionData?.auctionBid?.bid]);
-			setCurrentBid(subscriptionData?.auctionBid?.bid);
-			setStatus(subscriptionData?.auctionBid?.status);
-			console.log(subscriptionData);
-		}
-	}, [subscriptionData, data, auctionData]);
+    if (subscriptionData) {
+      console.log("subscription effect");
+      console.log(subscriptionData);
+      setAuctionData([...auctionData, subscriptionData?.auctionBid?.bid]);
+      setCurrentBid(subscriptionData?.auctionBid?.bid);
+      setStatus(subscriptionData?.auctionBid?.status);
+    }
+  }, [subscriptionData, data]);
 
-	const styles = {
-		divider: {
-			borderColor: "rgb(0 0 0 / 34%)",
-		},
-		biddingTitle: {
-			textAlign: "center",
-			textDecoration: "underline",
-			margin: 2,
-		},
-		biddingContainer: {
-			width: "100%",
-			display: "flex",
-			flexDirection: "column-reverse",
-			alignItems: "center",
-			gap: "1rem",
-		},
-	};
+  const styles = {
+    divider: {
+      borderColor: "rgb(0 0 0 / 34%)",
+    },
+    biddingTitle: {
+      textAlign: "center",
+      textDecoration: "underline",
+      margin: 2,
+    },
+    biddingContainer: {
+      width: "100%",
+      display: "flex",
+      flexDirection: "column-reverse",
+      alignItems: "center",
+      gap: "1rem",
+    },
+  };
 
-	if (error || loading) return <h1>Error</h1>;
+  if (error || loading) return <h1>Error</h1>;
 
-	console.log(subscriptionData);
-	return (
-		data?.getSingleListing && (
-			<>
-				<ListingItem listingId={id} data={data} currentBid={currentBid} />
-				<Divider sx={styles.divider} />
-				<Typography
-					gutterBottom
-					variant="h5"
-					component="div"
-					sx={styles.biddingTitle}
-				>
-					Bidding Activity
-				</Typography>
-				<Box sx={styles.biddingContainer}>
-					<List
-						sx={{
-							width: "100%",
-							maxWidth: 550,
-							bgcolor: "background.paper",
-							display: "flex",
-							flexDirection: "column-reverse",
-						}}
-						disablePadding
-					>
-						{/* Data on page load */}
-						{auctionData && auctionData.map((bid) => <BiddingCard bid={bid} />)}
-					</List>
-				</Box>
-			</>
-		)
-	);
+  return (
+    data?.getSingleListing && (
+      <>
+        <ListingItem
+          listingId={id}
+          data={data}
+          currentBid={currentBid}
+          status={status}
+        />
+        <Divider sx={styles.divider} />
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={styles.biddingTitle}
+        >
+          Bidding Activity
+        </Typography>
+        <Box sx={styles.biddingContainer}>
+          <List
+            sx={{
+              width: "100%",
+              maxWidth: 550,
+              bgcolor: "background.paper",
+              display: "flex",
+              flexDirection: "column-reverse",
+            }}
+            disablePadding
+          >
+            {auctionData && auctionData.map((bid) => <BiddingCard bid={bid} />)}
+          </List>
+        </Box>
+      </>
+    )
+  );
 };

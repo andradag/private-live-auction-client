8a8a1b565d642efc281028493e7c448c8dd55532
Asmaa <Asmaa99musse@gmail.com>
2022-03-24 13:38:18 +0000

    added border to styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,6 +9,7 @@ export const FooterStyling = () => {
     <footer>
       <Box
         sx={{
+          border: "solid",
           backgroundColor: "#E08604",
           color: "white",
           padding: "10px",

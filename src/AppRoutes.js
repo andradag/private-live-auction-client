ff5e4d950730678175af3a77d0ae5ddbe5d7110f
AkenEagle <akeneagle@gmail.com>
2022-03-25 20:43:37 +0000

    protected routes

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,24 +1,29 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import {Home} from "./pages/Home/";
-import {LoginSide} from "./pages/LoginSide";
-import {SignUpSide} from "./pages/SignUpSide";
-import {AuctionPage} from "./pages/AuctionPage";
-import {CreateAuctionForm} from "./pages/CreateAuction";
-import {SecondaryDashboardPage} from "./pages/SecondaryDashboard";
-import {HowToBid} from "./pages/HowToBid";
+import { Home } from "./pages/Home/";
+import { LoginSide } from "./pages/LoginSide";
+import { SignUpSide } from "./pages/SignUpSide";
+import { AuctionPage } from "./pages/AuctionPage";
+import { HouseForm } from "./pages/HouseForm";
+import { SecondaryDashboardPage } from "./pages/SecondaryDashboard";
+import { HowToBid } from "./pages/HowToBid";
+import { useAuth } from "./contexts/AppProvider";
 
 export const AppRoutes = () => {
-	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/dashboard" element={<SecondaryDashboardPage />} />
-			<Route path="/login" element={<LoginSide />} />
-			<Route path="/signup" element={<SignUpSide />} />
-			<Route path="/auction/:id" element={<AuctionPage />} />
-			<Route path="/how-to-bid" element={<HowToBid />} />
-			<Route path="/create-auction" element={<CreateAuctionForm />} />
-			<Route path="*" element={<Home />} />
-		</Routes>
-	);
+  const user = useAuth();
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/dashboard"
+        element={<SecondaryDashboardPage user={user} />}
+      />
+      <Route path="/login" element={<LoginSide />} />
+      <Route path="/signup" element={<SignUpSide />} />
+      <Route path="/auction/:id" element={<AuctionPage user={user} />} />
+      <Route path="/how-to-bid" element={<HowToBid />} />
+      <Route path="/create-auction" element={<HouseForm user={user} />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import Carousel from "../../components/pages/Home/Carousel";
 import Typography from "@mui/material/Typography";
 import "@fontsource/roboto/300.css";
-import Card from "@mui/material/Card";
-import Box from "@mui/material/Box";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
+import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+
+import "./index.css";
 
 const theme = createTheme({
   typography: {
@@ -47,61 +48,58 @@ export const Home = () => {
         Auction: Turing cash into assets
       </Typography>
 
-      <Box>
-        <Card sx={{ display: "flex", flexDirection: "row", marginTop: "10px" }}>
-          <CardMedia
+      <Container
+        className="gjfj"
+        sx={{
+          maxWidth: "90%",
+          my: 3,
+          mx: "auto",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <CardMedia
+          sx={{
+            height: 540,
+            width: 700,
+            my: 3,
+            boxShadow: "5",
+          }}
+          image="https://images.rics.org/publishedmedia/ihoyjek49pynihgcvp6d/RICS-Professional_Renewal.jpg"
+        />
+        <CardContent
+          sx={{
+            textAlign: "center",
+            fontWeight: "medium",
+            height: 500,
+            my: 3,
+            boxShadow: "5",
+            width: 700,
+          }}
+        >
+          <Typography
             sx={{
-              height: 540,
-              width: 700,
-              boxShadow: "7",
-              marginRight: "60px",
-              marginLeft: "100px",
-              marginTop: "60px",
-              marginBottom: "40px",
-            }}
-            image="https://images.rics.org/publishedmedia/ihoyjek49pynihgcvp6d/RICS-Professional_Renewal.jpg"
-          />
-
-          <CardContent
-            sx={{
-              // border: "solid",
-              textAlign: "center",
-              boxShadow: "7",
-              fontWeight: "medium",
-              height: 500,
-              width: 700,
-              marginRight: "100px",
-              marginLeft: "60px",
-              marginTop: "60px",
-              marginBottom: "40px",
+              fontFamily: "Ramaraja, serif",
+              fontSize: 20,
+              my: 2,
             }}
           >
-            <Typography
-              sx={{
-                fontFamily: "Ramaraja, serif",
-                fontSize: 20,
-                marginTop: "20px",
-                marginLeft: "30px",
-                marginRight: "30px",
-              }}
-            >
-              Established in 1894, Forbiddin' House is the world’s largest, most
-              trusted and dynamic marketplace for art and luxury. We empower our
-              international community of collectors and connoisseurs to
-              discover, acquire, finance and consign fine art and rare objects.
-              Our reputation for trust and authenticity is backed by our
-              unparalleled global network of specialists spanning 40 countries
-              and 44 departments, which include Contemporary Art, Modern and
-              Impressionist Art, Old Masters, Chinese Works of Art, jewelry,
-              watches, wine and spirits, and interiors, among many others.
-              Guided by our forward-thinking spirit of innovation, we host over
-              600 auctions annually and offer a cross-category selection of
-              items available for immediate purchase via both digital and
-              physical shopping experiences as well as private sales.{" "}
-            </Typography>
-          </CardContent>
-        </Card>
-      </Box>
+            Established in 1894, Forbiddin' House is the world’s largest, most
+            trusted and dynamic marketplace for art and luxury. We empower our
+            international community of collectors and connoisseurs to discover,
+            acquire, finance and consign fine art and rare objects. Our
+            reputation for trust and authenticity is backed by our unparalleled
+            global network of specialists spanning 40 countries and 44
+            departments, which include Contemporary Art, Modern and
+            Impressionist Art, Old Masters, Chinese Works of Art, jewelry,
+            watches, wine and spirits, and interiors, among many others. Guided
+            by our forward-thinking spirit of innovation, we host over 600
+            auctions annually and offer a cross-category selection of items
+            available for immediate purchase via both digital and physical
+            shopping experiences as well as private sales.{" "}
+          </Typography>
+        </CardContent>
+      </Container>
     </>
   );
 };
